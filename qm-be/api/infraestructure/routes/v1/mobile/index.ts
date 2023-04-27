403ae75f337de2270014a6c7396df062139ef32d
Cristian Chiappone <cristian.chiappone@mercadolibre.com>
2023-04-27 08:24:40 -0300

    Add products endpoints

--- qm-be/api/infraestructure/routes/v1/mobile/index.ts
+++ qm-be/api/infraestructure/routes/v1/mobile/index.ts
@@ -1,14 +1,25 @@
 import { Router } from "express";
 import getAllRestaurants from "../../../../interface/restaurants/getAll";
 import getRestaurant from "../../../../interface/restaurants/get";
-import getAllCategories from "../../../../interface/categories/getAll";
+import {
+  getAllCategories,
+  getAllCategoriesAndProducts
+} from "../../../../interface/categories/getAll";
 import getCategory from "../../../../interface/categories/get";
 
 const router = Router();
 
+// Restaurants
 router.get("/restaurants", getAllRestaurants);
 router.get("/restaurants/:restaurantId", getRestaurant);
 router.get("/restaurants/:restaurantId/categories", getAllCategories);
+router.get(
+  "/restaurants/:restaurantId/categories/products",
+  getAllCategoriesAndProducts
+);
 router.get("/restaurants/:restaurantId/categories/:categoryId", getCategory);
+
+// Orders
+router.post("/orders", getAllRestaurants);
 
 export default router;
