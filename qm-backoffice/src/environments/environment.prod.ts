7f6fbf3fbf24ba28ff5f832275e2e8cb53915eed
Cristian Chiappone <cristian.chiappone@mercadolibre.com>
2024-04-07 13:07:08 -0300

    Add infra yamls

--- qm-backoffice/src/environments/environment.prod.ts
+++ qm-backoffice/src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
+<<<<<<< HEAD
+  production: true
+  
+=======
   production: true,
   backendUrl: window['env'].BACKEND_URL
+>>>>>>> 85df06bb122b9277e9dbcb4f3d3c75ba7238e3bc
 };
