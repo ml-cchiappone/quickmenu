ba8c6a247a806e6c74c0eda1182d08e7e8a22e49
Cristian Chiappone <cristian.chiappone@mercadolibre.com>
2024-04-07 13:09:07 -0300

    solve conflict

--- qm-backoffice/src/environments/environment.prod.ts
+++ qm-backoffice/src/environments/environment.prod.ts
@@ -1,9 +1,4 @@
 export const environment = {
-<<<<<<< HEAD
-  production: true
-  
-=======
   production: true,
   backendUrl: window['env'].BACKEND_URL
->>>>>>> 85df06bb122b9277e9dbcb4f3d3c75ba7238e3bc
 };
